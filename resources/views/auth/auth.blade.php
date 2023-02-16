de82a108accd2e3c27f281bfca27708004eb201e
wanfahdiva <wanfahdiva07@gmail.com>
2023-02-16 16:21:44 +0700

    feat(initial): add assets & configuration path

---
+++ resources/views/auth/auth.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.auth.app')
+@section('title', 'E-Arsip || Masuk')
+@section('content')
+    <div class="d-flex align-items-center justify-content-center vh-100">
+        <div class="card shadow-lg border px-4 py-4 bg-white align-items-center" style="width: 393px;">
+            <div class="d-flex align-items-center justify-content-center">
+                <img src="{{ asset('images/logo.png') }}" class="card-img-top w-50 h-50" alt="...">
+            </div>
+            <h1 class="fs-5 fw-bolder text-body my-4 text-align-center">Masuk</h1>
+            <form class="w-100">
+                <div class="mb-3">
+                    <input type="email" class="form-control rounded" placeholder="Email">
+                </div>
+                <div class="mb-3">
+                    <input type="password" class="form-control rounded" placeholder="Kata Sandi">
+                </div>
+                <button type="submit" class="btn btn-primary w-100">Masuk</button>
+            </form>
+        </div>
+    </div>
+@endsection
